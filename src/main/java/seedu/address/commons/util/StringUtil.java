8b22b0ec0015333d1a1aef4d52e0e841568e067e
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-15 00:55:35 +0800

    StringUtil: Create more string methods
    The following StringUtil methods are created:
    * matchesWordInSetIgnoreCase: Returns true if any of the words in the
    wordSet exactly matches the search word.
    * matchesSubStringInSetIgnoreCase: Returns true if any of the substrings in the
    wordSet exactly matches the search word.
    * fuzzyMatchesWordInSetIgnoreCase: Returns true if any of the words in
    the wordSet fuzzily matches the search word.
    * fuzzyMatch: Returns true if the search fuzzily matches two
    strings
    
    These methods would be crucial for the logic of FindCommand.

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -6,6 +6,9 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.Set;
+
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 /**
  * Helper functions for handling strings.
@@ -36,6 +39,99 @@ public class StringUtil {
 
         return Arrays.stream(wordsInPreppedSentence)
                 .anyMatch(preppedWord::equalsIgnoreCase);
+    }
+
+    /**
+     * Returns true if any of the words in the {@code wordSet} exactly matches the {@code word}.
+     * <br></br>
+     * Ignores case, a full word match is required.
+     * @param word word to be checked against the wordSet, cannot be null or empty
+     * @param wordSet set of words to be checked against the word, cannot be null or empty
+     * @return true if any of the words in the wordSet is an exact match, false otherwise
+     */
+    public static boolean matchesWordInSetIgnoreCase(String word, Set<String> wordSet) {
+        requireNonNull(word);
+        requireNonNull(wordSet);
+
+        String preppedWord = word.toLowerCase().trim();
+        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(!wordSet.isEmpty(), "Word set cannot be empty");
+
+        return wordSet.stream()
+                .map(k -> k.toLowerCase().trim())
+                .anyMatch(preppedWord::equals);
+    }
+
+    /**
+     * Returns true if any of the words in the {@code wordSet} contains the {@code word}.
+     * <br></br>
+     * Ignores case, only a substring match is required.
+     * @param word word to be checked against the wordSet, cannot be null or empty
+     * @param wordSet set of words to be checked against the word, cannot be null or empty
+     * @return true if any of the words in the wordSet contains word as a substring, false otherwise
+     */
+    public static boolean matchesSubstringInSetIgnoreCase(String word, Set<String> wordSet) {
+        requireNonNull(word);
+        requireNonNull(wordSet);
+
+        String preppedWord = word.toLowerCase().trim();
+        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(!wordSet.isEmpty(), "Word set cannot be empty");
+
+        return wordSet.stream()
+                .map(k -> k.toLowerCase().trim())
+                .anyMatch(preppedWord::contains);
+    }
+
+    /**
+     * Returns true if any of the words in the {@code wordSet} fuzzy matches the {@code word}.
+     * <br></br>
+     * Ignores case, fuzzy match is done through the Levenshtein distance
+     * algorithm in {@link #fuzzyMatch(String, String)}.
+     */
+    public static boolean fuzzyMatchesWordInSetIgnoreCase(String word, Set<String> wordSet) {
+        requireNonNull(word);
+        requireNonNull(wordSet);
+
+        String preppedWord = word.toLowerCase().trim();
+        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(!wordSet.isEmpty(), "Word set cannot be empty");
+
+        return wordSet.stream()
+                .map(k -> k.toLowerCase().trim())
+                .anyMatch(k -> fuzzyMatch(preppedWord, k));
+    }
+
+    /**
+     * Returns {@code true} if the two strings are similar enough to be considered a match.
+     * <br>
+     * Exact matches are always returned as true.
+     * For strings longer than 2 characters, a Levenshtein distance of up to 2 edits is allowed for small typos.
+     * Read more about Levenshtein distance <a href="https://en.wikipedia.org/wiki/Levenshtein_distance">here</a>
+     *
+     * @param s1 String to compare
+     * @param s2 String to compare
+     * @return true if the strings match exactly or fall within the typo threshold
+     */
+    private static boolean fuzzyMatch(String s1, String s2) {
+        requireNonNull(s1);
+        requireNonNull(s2);
+
+        checkArgument(!s1.isEmpty(), "Word parameter cannot be empty");
+        checkArgument(!s2.isEmpty(), "Word parameter cannot be empty");
+
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        if (s1.length() <= 2 || s2.length() <= 2) {
+            return false;
+        }
+
+        LevenshteinDistance levenshtein = new LevenshteinDistance(2);
+        Integer distance = levenshtein.apply(s1, s2);
+
+        return distance != null && distance != -1;
     }
 
     /**

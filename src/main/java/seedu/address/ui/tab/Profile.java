dec5fbf595001c017a387e6894fef591039253f7
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-23 04:14:14 +0800

    Ui: Add Profile

---
+++ src/main/java/seedu/address/ui/tab/Profile.java
@@ -0,0 +1,13 @@
+package seedu.address.ui.tab;
+
+import javafx.scene.layout.Region;
+import seedu.address.ui.UiPart;
+
+public class Profile extends UiPart<Region> {
+
+    private static final String FXML = "Profile.fxml";
+
+    public Profile() {
+        super(FXML);
+    }
+}

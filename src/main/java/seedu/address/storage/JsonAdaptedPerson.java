aa6e44ecd5da233b30c7c835a24c4f82ed963de6
Keng How <khchin0901@gmail.com>
2026-03-23 18:37:58 +0800

    Fix JsonAdaptedPerson checkstyle violation

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -40,9 +40,12 @@ class JsonAdaptedPerson {
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-                             @JsonProperty("email") String email, @JsonProperty("studentId") String studentId,
-                             @JsonProperty("roomNumber") String roomNumber, @JsonProperty("emergencyContact") String emergencyContact,
+    public JsonAdaptedPerson(@JsonProperty("name") String name,
+                             @JsonProperty("phone") String phone,
+                             @JsonProperty("email") String email,
+                             @JsonProperty("studentId") String studentId,
+                             @JsonProperty("roomNumber") String roomNumber,
+                             @JsonProperty("emergencyContact") String emergencyContact,
                              @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
